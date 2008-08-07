d7a838c69b8d6a80c3fe6f1f3b2b8d2ba38cc12c
chiselwright <chiselwright@d010b805-6333-0410-82cb-f46a6febb1e3>
2008-08-07 10:58:12 +0000

    r4983@wiggin:  chisel | 2008-04-18 22:41:28 +0100  / deprecated at mst's request until it can be written sanely

---
+++ tags/Catalyst-Plugin-Dumper-0.00005/lib/Catalyst/Plugin/Dumper.pm
@@ -0,0 +1,39 @@
+package Catalyst::Plugin::Dumper;
+# vim: ts=8 sts=4 et sw=4 sr sta
+use warnings;
+use strict;
+use 5.8.1;
+
+our $VERSION = '0.00005';
+
+1; # Magic true value required at end of module
+__END__
+
+=pod
+
+=head1 NAME
+
+Catalyst::Plugin::Dumper - (Deprecated) Needs implementing sanely
+
+=head1 VERSION
+
+This document describes Catalyst::Plugin::Dumper version 0.0.1
+
+=head1 DESCRIPTION
+
+Previous versions of this plugin injects a dumper() method into the
+L<Catalyst::Log> namespace.
+
+This has always been Evil and Wrong.
+
+It needs writing correctly.
+
+Until then, this module is deprecated.
+
+Sorry.
+
+=head1 AUTHOR
+
+Chisel Wright  C<< <pause@herlpacker.co.uk> >>
+
+=cut
